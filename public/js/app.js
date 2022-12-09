4f41cbdb6ea44d65f2c4394ed46b735ba80b15e8
Milt Deherrera <miltdeherrera@gmail.com>
2022-12-08 17:20:21 -0800

    Refactor backend to use scripts in utils folder. Set up app for Heroku deployment

--- public/js/app.js
+++ public/js/app.js
@@ -10,19 +10,18 @@ weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
   
   const location = search.value
-  const url = 'http://api.weatherstack.com/current?access_key=4198362410415db3501b1199a7297701&query=' + location + '&units=f'
 
   messageOne.textContent = 'Loading...'
   messageTwo.textContent = ''
   
-  fetch(url).then((response) => {
+  fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
       if (data.error) {
-        messageOne.textContent = data.error.info
+        messageOne.textContent = data.error
         messageTwo.textContent = ''
       } else {
-        messageOne.textContent = data.request.query
-        messageTwo.textContent = data.current.temperature + ' degrees Fahrenheit'
+        messageOne.textContent = data.location
+        messageTwo.textContent = data.forecast
       }
     })
   })
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,8 +3,6 @@ const request = require('request')
 const geocode = (address, callback) => {
   const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibWRlaGVycmVyYSIsImEiOiJjbGJkd3ZlOGMwNW5uM25tZmdrNjdmY3I2In0.9Gsby3FDw3iZpWcGViw4UA&limit=1'
 
-  console.log(url)
-
     request({ url, json: true }, (error, { body }) => {
       if (error) {
         return callback('Unable to connect to Mapbox API')
